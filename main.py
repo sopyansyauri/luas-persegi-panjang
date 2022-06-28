82e8e6e5e59787d44cab9c3f43e1a3effdc3ef56
sopyan04 <rifai.sopyan04@gmail.com>
2022-06-28 19:33:15 +0700

    program luas persegi panjang

---
+++ main.py
@@ -0,0 +1,9 @@
+print("LUAS PERSEGI PANJANG")   
+
+panjang = int(input("masukkan angka panjangnya: "))
+lebar = int(input("masukkan angka lebarnya: "))
+
+luas = panjang * lebar
+
+print("luasnya adalah:", luas)
+# print(luas)
